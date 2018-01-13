c126db8db09af1ec187d89e5aa60f302b6a7747b
Bakyt <bbalbakov@askartec.com>
2018-01-13 19:10:19 +0600

    added test "should convert array values to Promises"

--- test/valuesToPromises.js
+++ test/valuesToPromises.js
@@ -13,4 +13,16 @@ describe('"check" valuesToPromises function', function () {
     it('is a function', function () {
         expect(valuesToPromises).to.be.a('function');
     });
+
+    it('should convert array values to Promises', function () {
+        let noPromiseValues = [1, 5];
+        let promiseValues = valuesToPromises(noPromiseValues);
+        expect(promiseValues.toString()).to.equal('[object Promise],[object Promise]');
+    });
+
+    it('should convert array non Promise values to Promises', function () {
+        let noPromiseValues = [Promise.resolve(2), 5];
+        let promiseValues = valuesToPromises(noPromiseValues);
+        expect(promiseValues.toString()).to.equal('[object Promise],[object Promise]');
+    });
 });
