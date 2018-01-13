a30ea80f83f0ad160a5f5dccf35bb31013c231b9
Bakyt <bbalbakov@askartec.com>
2018-01-13 18:32:52 +0600

    added isPromise function tests

---
+++ test/functions.js
@@ -0,0 +1,41 @@
+import {
+    isPromise,
+    hasPromiseValues
+} from '../lib/immunitet';
+import Chai from 'chai';
+const {
+    expect,
+    assert,
+    should,
+} = Chai;
+
+describe('"check" isPromise function', function () {
+
+    it('is a function', function () {
+        expect(isPromise).to.be.a('function');
+    });
+
+    it('should return false if an argument is a falsy value', function () {
+        expect(isPromise(NaN)).to.equal(false);
+        expect(isPromise(false)).to.equal(false);
+        expect(isPromise(undefined)).to.equal(false);
+        expect(isPromise(null)).to.equal(false);
+        expect(isPromise(0)).to.equal(false);
+        expect(isPromise('')).to.equal(false);
+    });
+
+    it('should return false if an argument is not Promise', function () {
+        expect(isPromise({})).to.equal(false);
+        expect(isPromise(1)).to.equal(false);
+        expect(isPromise('Promise')).to.equal(false);
+        expect(isPromise({then: () => { console.log('TEST log!!!') }})).to.equal(false);
+        expect(isPromise([])).to.equal(false);
+        expect(isPromise({toString: () => '[object Promise]'})).to.equal(false);
+    });
+
+    it('should return true if an object is Promise', function () {
+        let promise = new Promise(() => {}, () => {});
+
+        expect(isPromise(promise)).to.equal(true);
+    });
+});
