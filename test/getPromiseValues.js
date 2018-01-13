3255f13a89115134cd42476277d788437ba86bee
Bakyt <bbalbakov@askartec.com>
2018-01-13 19:39:55 +0600

    fixed test case "should resolve array of Promises and get values"

--- test/getPromiseValues.js
+++ test/getPromiseValues.js
@@ -14,4 +14,17 @@ describe('"check" getPromiseValues function', function () {
         expect(getPromiseValues).to.be.a('function');
     });
 
+    it('should resolve array of Promises and get values', function () {
+        let mixedValues = [2, Promise.resolve(5)];
+        let resolveValues = getPromiseValues(mixedValues);
+        resolveValues
+            .then(result => {
+                expect(result.join(',')).to.equal('2,5');
+            })
+            .catch(error => {
+                console.error('error:', error);
+                expect(error).to.equal(null);
+            })
+    });
+
 });
