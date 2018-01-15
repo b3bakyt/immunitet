35e2dd94e1da5866dc92485632e28781495f893d
Bakyt <bbalbakov@askartec.com>
2018-01-15 16:50:39 +0600

    added test "should properly run if object type argument processor is given"

--- test/object_pattern_processors.js
+++ test/object_pattern_processors.js
@@ -8,8 +8,22 @@ const {
 } = Chai;
 
 describe('check object type pattern processors', function () {
+    function add(a, b) {
+        return a + b;
+    }
 
     it('is function', function () {
         expect(processObjectPatterns).to.be.a('function');
+    });
+
+    it('should properly run if object type argument processor is given', function () {
+        let checkAdd = check(add, {
+            a: {
+                type: 'number',
+            },
+        });
+
+        let [result, error] = checkAdd("33", 2);
+        expect(result).to.equal(35);
     });
 });
