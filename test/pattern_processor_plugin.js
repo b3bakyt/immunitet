fb4ad014a99f26fc47f56edceb985cdf034852a6
Bakyt <bbalbakov@askartec.com>
2018-01-15 13:11:55 +0600

    added test "pluginPatternProcessors func. should return an object"

--- test/pattern_processor_plugin.js
+++ test/pattern_processor_plugin.js
@@ -11,4 +11,9 @@ describe('check plugable pattern processors', function () {
     it('pluginPatternProcessors is a function', function () {
         expect(pluginPatternProcessors).to.be.a('function');
     });
+
+    it('pluginPatternProcessors func. should return an object', function () {
+        const pathToPlugins = './data/pattern_plugins';
+        expect(pluginPatternProcessors(pathToPlugins)).to.be.an('object');
+    });
 });
