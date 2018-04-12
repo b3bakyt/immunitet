3220265500ab1f2b6c81f147c1bd7ef95bb38352
b3bakyt <b3bakyt@gmail.com>
2018-04-12 22:21:36 +0600

    test - "pattern" pattern, given empty value should return error

--- test/string_pattern_processors.js
+++ test/string_pattern_processors.js
@@ -773,3 +773,31 @@ describe('check "object" pattern processor', function () {
 
     });
 });
+
+describe('check "pattern" pattern processor', function () {
+    let checkAdd = null;
+    function invert(b) {
+        return !b;
+    }
+
+    it('given empty values should return error', function () {
+        checkAdd = check(invert, 'pattern');
+        let [, error1] = checkAdd('');
+        expect(error1).not.equal(null);
+
+        let [, error2] = checkAdd(NaN);
+        expect(error2).not.equal(null);
+
+        let [, error3] = checkAdd(null);
+        expect(error3).not.equal(null);
+
+        let [, error4] = checkAdd(0);
+        expect(error4).not.equal(null);
+
+        let [, error5] = checkAdd(undefined);
+        expect(error5).not.equal(null);
+
+        let [, error6] = checkAdd(false);
+        expect(error6).not.equal(null);
+    });
+});
