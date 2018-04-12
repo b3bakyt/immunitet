ab5cc3a6bed5fcbe41ba396148c4bbaa2f249871
b3bakyt <b3bakyt@gmail.com>
2018-04-12 18:52:38 +0600

    test - object pattern, given non Object value should return error

--- test/string_pattern_processors.js
+++ test/string_pattern_processors.js
@@ -716,4 +716,35 @@ describe('check "object" pattern processor', function () {
         let [, error6] = checkAdd(false);
         expect(error6).not.equal(null);
     });
+
+    it('given a non "object" value should return error', function () {
+        checkAdd = check(invert, 'object');
+
+        let [, error1] = checkAdd(22);
+        expect(error1).not.equal(null);
+
+        let [result2, error2] = checkAdd(Infinity);
+        expect(result2).to.equal(null);
+        expect(error2).not.equal(null);
+
+        let [result3, error3] = checkAdd('hi');
+        expect(result3).to.equal(null);
+        expect(error3).not.equal(null);
+
+        let [result5, error5] = checkAdd(2.2);
+        expect(result5).to.equal(null);
+        expect(error5).not.equal(null);
+
+        let [result6, error6] = checkAdd(false);
+        expect(result6).to.equal(null);
+        expect(error6).not.equal(null);
+
+        let [result7, error7] = checkAdd([]);
+        expect(result7).to.equal(null);
+        expect(error7).not.equal(null);
+
+        let [result8, error8] = checkAdd([1,2,3,44]);
+        expect(result8).to.equal(null);
+        expect(error8).not.equal(null);
+    });
 });
