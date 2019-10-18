0c856170fe7f7a0658c1eac4ee585cdfb243553c
Bakyt B <b3bakyt@gmail.com>
2019-10-18 12:20:45 +0600

    test failed - should throw eng errors by default

---
+++ test/translations.test.js
@@ -0,0 +1,29 @@
+const {
+    setLanguage,
+    validateValue,
+    validatePromise,
+    validateFunction,
+    ImmunitetException,
+} = require('../src/immunitet');
+
+const Chai = require('chai');
+
+const {
+    expect,
+    assert,
+    should,
+} = Chai;
+
+setLanguage('eng');
+
+describe('check "translations" for correct work ', function () {
+
+    it('should throw eng errors by default', function () {
+        try {
+            let noValidFunction = validateFunction('test');
+        } catch (error) {
+            expect(error.message).to.equal('First argument must be a type of function or null!');
+        }
+    });
+
+});
